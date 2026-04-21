b505517aabb8a34586b44ac1557e64a09ed68b65
azamatkhu <azamat.khu@educa.jcyl.es>
2026-04-21 13:19:11 +0200

    Bulk upload

--- src/MainActualizarEmpleado.java
+++ src/MainActualizarEmpleado.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.sql.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class MainActualizarEmpleado {
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1521:xe",
+                    "RIBERA",
+                    "ribera"
+            );
+
+            System.out.println("Conectado!");
+
+            Statement statement = connection.createStatement();
+
+            String sql = "UPDATE EMPLEADO SET SALARIO = SALARIO + ?";
+
+            PreparedStatement ps = connection.prepareStatement(sql);
+
+            ps.setDouble(1, 99999);
+            ps.executeUpdate();
+
+            System.out.println("Actualizado!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
